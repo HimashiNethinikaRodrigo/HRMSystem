165479536a0d1e1e9f78dace5b1f3dda2994fa35
HimashiNethinikaRodrigo <himashirodrigo23@gmail.com>
2020-11-26 13:49:11 +0100

    add DepartmentDAO

---
+++ src/main/java/com/hrmanagement/dao/DepartmentDAO.java
@@ -0,0 +1,9 @@
+package com.hrmanagement.dao;
+
+import com.hrmanagement.domain.Department;
+
+import java.util.List;
+
+public interface DepartmentDAO {
+    List<Department> getAllDepartments();
+}
