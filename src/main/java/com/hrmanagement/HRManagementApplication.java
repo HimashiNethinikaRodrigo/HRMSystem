f5da2efe5f93652b80bd78dbbe7ff028ec8a7f30
HimashiNethinikaRodrigo <himashirodrigo23@gmail.com>
2020-11-26 13:49:45 +0100

    add application class

---
+++ src/main/java/com/hrmanagement/HRManagementApplication.java
@@ -0,0 +1,13 @@
+package com.hrmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HRManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HRManagementApplication.class, args);
+    }
+
+}
